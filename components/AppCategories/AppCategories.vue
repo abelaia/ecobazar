c281f62ba626c8ffff53f0f52dc89ce6767bca56
Алина <abelaia@sfedu.ru>
2026-04-17 12:54:03 +0300

    section-header добавила в ui

--- components/AppCategories/AppCategories.vue
+++ components/AppCategories/AppCategories.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AppButton from '../UI/AppButton.vue';
+import AppSectionHeader from '../UI/AppSectionHeader/AppSectionHeader.vue';
 import CategoryCard from '@/components/AppCategoryCard/AppCategoryCard.vue';
 import { categories } from '@/constants/categories.js';
 </script>
@@ -7,16 +7,7 @@ import { categories } from '@/constants/categories.js';
 <template>
     <div class="home-categories">
         <div class="home-categories__container">
-            <div class="home-categories__header">
-                <h2 class="home-categories__title">
-                    Popular Categories
-                </h2>
-                <AppButton 
-                    class="home-categories__button"
-                    text="View All"
-                    :has-wrapper="false"
-                />
-            </div>
+            <AppSectionHeader />
             <div class="home-categories__grid">
                 <CategoryCard
                     v-for="category in categories"
---
+++ components/UI/AppSectionHeader/AppSectionHeader.vue
@@ -0,0 +1,46 @@
+<script setup>
+import AppButton from '../AppButton.vue';
+</script>
+
+<template>
+    <div class="section-header">
+        <h2 class="section-header__title">
+            Popular Categories
+        </h2>
+        <AppButton 
+            class="section-header__button"
+            text="View All"
+            :has-wrapper="false"
+        />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/vars.scss' as *;
+
+.section-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    &__title {
+        font-family: $font-primary;
+        font-size: $font-size-heading;
+        font-weight: $font-weight-semibold;
+        color: $color-gray-900;
+    }
+
+    &__button {
+        font-family: $font-primary;
+        font-size: $font-size-m;
+        font-weight: $font-weight-medium;
+        color: $color-green-success;
+        cursor: pointer;
+        transition: color 0.3s ease;
+
+        &:hover {
+            color: $color-green-dark;
+        }
+    }
+}
+</style>
